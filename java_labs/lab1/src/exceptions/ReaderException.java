35c5c98400b08dd36bb949578552034028816a95
danil_kuzin <dnkuzzin@mail.ru>
2026-02-09 23:37:34 +0700

    Remove java_labs folder

--- java_labs/lab1/src/exceptions/ReaderException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-import java.io.IOException;
-
-public class ReaderException extends IOException {
-    public ReaderException(String message, Throwable cause) {
-        super("[READ ERROR]: " +message, cause);
-    }
-}
